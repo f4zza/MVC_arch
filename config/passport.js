ee013bbb80fe91862c582a4cc8b6bd7b909da86b
f4zza <lazie.piscean@live.co.uk>
2023-03-04 13:11:07 +0300

    fixes

--- config/passport.js
+++ config/passport.js
@@ -2,6 +2,7 @@ const LocalStrategy = require('passport-local').Strategy
 const mongoose = require('mongoose')
 const User = require('../models/User')
 const { OAuth2Strategy: GoogleStrategy } = require('passport-google-oauth');
+const refresh = require('passport-oauth2-refresh');
 
 module.exports = function (passport) {
   passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
